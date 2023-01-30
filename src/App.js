01a13d589bda2fe9253b950dd7bc29e85bf84f63
Mark Moreno <markmoreno34@gmail.com>
2023-01-30 11:18:19 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
+  const key = process.env.WEATHER_KEY || "Failed";
+
+  console.log(process.env.WEATHER_KEY);
+
   return (
     <div className="flex justify-center items-center font-bold text-5xl w-full h-screen">
-      {process.env.REACT_WEATHER_KEY || "Failed"}
+      {key}
     </div>
   );
 }
