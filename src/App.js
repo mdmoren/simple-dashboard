a14d0cf2debf44dbb05aeb8d2b3f996ebf3717fd
Mark Moreno <markmoreno34@gmail.com>
2023-01-30 12:52:08 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  console.log(process.env.REACT_TEST_KEY);
+  console.log(`${process.env.REACT_APP_TEST_KEY}`);
 
   return (
     <div className="flex justify-center items-center font-bold text-5xl w-full h-screen">
-      {process.env.REACT_TEST_KEY}
+      {`${process.env.REACT_APP_TEST_KEY}`}
     </div>
   );
 }
